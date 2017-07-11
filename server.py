8d453c9af629032c3f1d80f91a425c3476489331
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-11 15:17:22 -0700

    index displays entries in database in appropriate format

--- server.py
+++ server.py
@@ -6,8 +6,12 @@ mysql = MySQLConnector(app, 'fullfriends')
 
 @app.route('/')
 def index():
-    query = 'SELECT * FROM friends'
+    query = "SELECT CONCAT_WS(' ', first_name, last_name) AS 'name', age, DATE_FORMAT(created_at, '%M %D') AS month_day, YEAR(created_at) AS year FROM friends"
     friends = mysql.query_db(query)
     return render_template('index.html', friends=friends)
+
+@app.route('/add_friend', methods=['POST'])
+def add_friend():
+    return redirect('/')
 
 app.run(debug=True)
